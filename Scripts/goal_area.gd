29301d8e87cd1055a6f9596dc0b002817355f14f
studioai <studioai@hanmail.net>
2026-02-01 16:26:44 +0900

    何とか修正

--- Scripts/goal_area.gd
+++ Scripts/goal_area.gd
@@ -12,4 +12,4 @@ func _on_body_entered(body: Node2D):
 		# 충돌을 끄지 않으면 페이드아웃 중에 여러 번 호출될 수 있음
 		$CollisionShape2D.set_deferred("disabled", true)
 		
-		# 게임 매니저에게 "다음 스테이지로 가자"		GameManager.go_to_next_stage()
+		GameManager.go_to_next_stage()
